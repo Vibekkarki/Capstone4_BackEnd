069f67140e3e0519e5563f2750df8420e7951c6d
hanipatel <hanipatel042@gmail.com>
2024-10-08 20:18:57 -0400

    added email field for user table

--- models/user.js
+++ models/user.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
     type: String,
     required: true,
     unique: true,
