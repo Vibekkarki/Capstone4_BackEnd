d3921ffc950794727d80df35c0656e77343d11bb
hanipatel <hanipatel042@gmail.com>
2024-11-12 14:21:40 -0500

    created contraoller for dashboard

---
+++ controllers/dashboard/get_data_count.js
@@ -0,0 +1,11 @@
+const Task = require("../../models/Task");
+const Card = require("../../models/Card");
+const Board = require("../../models/board");
+const BoardMember = require("../../models/BoardMember");
+
+module.exports = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
