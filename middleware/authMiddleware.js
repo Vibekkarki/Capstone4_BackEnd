aade71e48802eb78b98fe1df288f13faf27d2521
hanipatel <hanipatel042@gmail.com>
2024-11-18 14:00:33 -0500

    resolved error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,17 @@
 const User = require("../models/user");
 
 module.exports = async (req, res, next) => {
+  console.log("Session data:", req.session); // Debug session data
+  if (!req.session.userId) {
+    return res
+      .status(401)
+      .json({ msg: "Access denied. Please sign in to continue." });
+  }
   const user = await User.findById(req.session.userId);
   if (!user) {
     return res
       .status(401)
       .json({ msg: "Access denied. Please sign in to continue." });
-  } else {
-    // If user is authenticated, continue to the next middleware or controller
-    next();
   }
+  next();
 };
