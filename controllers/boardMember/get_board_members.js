ddda734f96663f04c5815132b49fdc85c93fbb1a
Vibekkarki <58102435+Vibekkarki@users.noreply.github.com>
2024-11-18 19:04:27 -0500

    fixed userId and email issues

--- controllers/boardMember/get_board_members.js
+++ controllers/boardMember/get_board_members.js
@@ -1,6 +1,5 @@
 const User = require("../../models/user");
 const BoardMember = require("../../models/BoardMember");
-
 module.exports = async (req, res) => {
   try {
     const { boardId } = req.params;
@@ -16,15 +15,23 @@ module.exports = async (req, res) => {
       user_id: { $exists: true },
     }).select("user_id invite_email");
 
-    let formattedMembers = [];
+    // Collect user_ids from board members
+    const userIds = boardMembers.map((member) => member.user_id);
 
-    if (boardMembers.length > 0) {
-      formattedMembers = boardMembers.map((member) => ({
-        user_id: member.user_id,
-        email: member.invite_email,
-      }));
-    }
+    // Fetch emails for all user_ids
+    const users = await User.find({ _id: { $in: userIds } }).select("email");
+    const userMap = users.reduce((map, user) => {
+      map[user._id.toString()] = user.email;
+      return map;
+    }, {});
 
+    // Format members with emails
+    const formattedMembers = boardMembers.map((member) => ({
+      user_id: member.user_id,
+      email: userMap[member.user_id.toString()] || member.invite_email,
+    }));
+
+    // Add the logged-in user
     formattedMembers.push({
       user_id: userId,
       email: loggedInUser.email,
